6b2528c80cfe07b8bd9c2a5b4149e33b27dcc10e
Ashwini <076bct012.ashwini@pcampus.edu.np>
2022-02-15 17:40:22 +0545

    insertion sort added

--- Lab9/insertionsort.cpp
+++ Lab9/insertionsort.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 void insertionSort(int arr[], int n)
 {
+	for (int i = 1; i < n; i++)
+	{
+		int temp = arr[i];
+		int j = i - 1;
+		for (j = i - 1; j >= 0 && arr[j] > temp; j--)
+		{
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = temp;
+	}
+	cout << "The sorted array is:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
 }
 int main()
 {
