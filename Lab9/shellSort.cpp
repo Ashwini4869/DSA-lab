4d6e6efad7751c65663064facaad6d3fdb59d45a
Ashwini <076bct012.ashwini@pcampus.edu.np>
2022-03-05 15:22:23 +0545

    added radix sort

--- Lab9/shellSort.cpp
+++ Lab9/shellSort.cpp
@@ -1,39 +1,42 @@
-// unidentified error during sorting
 #include <iostream>
 using namespace std;
+
 int shellSort(int arr[], int n)
 {
 	for (int gap = n / 2; gap > 0; gap /= 2)
 	{
-		for (int i = gap; i < n; i++)
+		for (int i = gap; i < n; i += 1)
 		{
 			int temp = arr[i];
+
 			int j;
-			for (j = 1; (j >= gap && arr[j - gap]) > temp; j -= gap)
-			{
+			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
 				arr[j] = arr[j - gap];
-			}
+
 			arr[j] = temp;
 		}
 	}
 	return 0;
 }
-void displayArray(int arr[], int n)
+
+void printArray(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
+		cout << arr[i] << " ";
 }
+
 int main()
 {
-	int arr[] = {3, 5, 2, 1}, i;
+	int arr[] = {4, 5, 6, 2, 1, 3}, i;
 	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Array: \n";
-	displayArray(arr, n);
+
+	cout << "Array before sorting: \n";
+	printArray(arr, n);
+
 	shellSort(arr, n);
-	cout << "Sorted Array: \n";
-	displayArray(arr, n);
+
+	cout << "\nArray after sorting: \n";
+	printArray(arr, n);
+
 	return 0;
 }
